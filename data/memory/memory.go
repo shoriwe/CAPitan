20bcf61b5df0b2c8899b352772ce81df0491c576
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2021-11-08 09:53:33 -0500

    Unit test for the login page

--- data/memory/memory.go
+++ data/memory/memory.go
@@ -9,7 +9,8 @@ import (
 )
 
 type Memory struct {
-	users map[string]*objects.User
+	users  map[string]*objects.User
+	nextId uint
 }
 
 func (memory *Memory) Login(username, password string) (*objects.User, error) {
@@ -20,25 +21,32 @@ func (memory *Memory) Login(username, password string) (*objects.User, error) {
 	p := sha512.New()
 	p.Write([]byte(password))
 	passwordHash := p.Sum(nil)
-	if hex.EncodeToString(passwordHash) == result.PasswordHash {
-		return result, nil
+	if hex.EncodeToString(passwordHash) != result.PasswordHash {
+		return nil, nil
 	}
-	return nil, nil
+	if !result.IsEnabled {
+		return nil, nil
+	}
+	if time.Now().After(result.PasswordExpirationDate) {
+		return nil, nil
+	}
+	return result, nil
 }
 
 func NewInMemoryDB() data.Database {
 	result := &Memory{
-		users: map[string]*objects.User{},
+		users:  map[string]*objects.User{},
+		nextId: 2,
 	}
 	result.users["admin"] = &objects.User{
 		Id:                     1,
 		Username:               "admin",
-		PasswordHash:           "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec",
+		PasswordHash:           "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec", // admin
 		SecurityQuestion:       "Respond this with \"admin\"",
-		SecurityQuestionAnswer: "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec",
+		SecurityQuestionAnswer: "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec", // admin
 		IsAdmin:                true,
 		IsEnabled:              true,
-		PasswordExpirationDate: time.Now().Add(24 * time.Hour),
+		PasswordExpirationDate: time.Now().Add(30 * time.Minute),
 	}
 	return result
 }
---
+++ test/login_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestLogin(t *testing.T) {
+	server := NewTestServer()
+	defer server.Close()
+	client := server.Client()
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+	response, requestError := client.PostForm(
+		server.URL+"/login",
+		url.Values{
+			"username": []string{"admin"},
+			"password": []string{"admin"},
+		},
+	)
+	if requestError != nil {
+		t.Fatal(requestError)
+	}
+	if response.StatusCode != http.StatusFound {
+		t.Fatal(response)
+	}
+	location, err := response.Location()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if location.Path != "/dashboard" {
+		t.Fatal(location.Path)
+	}
+}
